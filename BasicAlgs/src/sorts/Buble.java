6edc9eea196c0e2c41758d8d81fc6c5f934268c5
«G.S.» <qwertyk@bk.ru>
2015-01-22 15:54:27 +0300

    Sort buble

--- BasicAlgs/src/sorts/Buble.java
+++ BasicAlgs/src/sorts/Buble.java
@@ -3,6 +3,31 @@ package sorts;
 public class Buble {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		int[] arr = {4, 3, 65, 5, 8,2, 7, 0};
+		arr = sort(arr);
+	}
+	
+	private static int[] sort (int[] arr){
+		int k = 0;
+		for (int i = 0; i < arr.length; i++) {
+	        for (int j = arr.length-1; j >= i ; j--) {
+	        	k++;
+	            System.out.println("Iteration: " + k + "\nElements: " + arr[i] + "vs" +arr[j]);
+	            if (arr[i] < arr[j]) swap(arr, i, j);
+	            printArray(arr);
+	        }
+		}
+		return arr;
+	}
+	
+	private static void swap(int[] arr, int i, int j){
+        System.out.println("Change: " + arr[i] + "<->" +arr[j]);
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;		
+	}
+	
+	private static void printArray(int[] arr) {
+		System.out.println(java.util.Arrays.toString(arr));
 	}
 }
